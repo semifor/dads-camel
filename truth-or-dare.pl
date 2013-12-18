2631d764c44e750a6df604211c35b42cb52d89ff
Marc Mims <marc@questright.com>
2013-12-18 10:50:22 -0800

    truthiness ...

--- truth-or-dare.pl
+++ truth-or-dare.pl
@@ -10,3 +10,4 @@ say "an empty string is false" unless '';
 say "an empty list is false"   unless ();
 say "the number 0 is false"    unless 0;
 say "the number 0e0 is false"  unless 0e0;
+say 'the string "0" is false'  unless "0";
