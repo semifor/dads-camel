ed2d8dfc9f53c3896c32e35540953120f9cfe6bb
Marc Mims <marc@questright.com>
2013-12-18 08:19:50 -0800

    sigil: scalar

---
+++ sigils.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use 5.12.1;
+use warnings;
+
+# The term sigil derives from the Latin sigillum, meaning "seal", though it may
+# also be related to the Hebrew סגולה (segula meaning "word, action, or item of
+# spiritual effect, talisman"). The current use of the term is derived from
+# Renaissance magic, which was in turn inspired by the magical traditions of
+# antiquity. - http://en.wikipedia.org/wiki/Sigil_(magic)
+
+my $name = 'perl';
+say "My name is $name.";
